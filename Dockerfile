8510c44bbe55b3829f7eb58af1d4716f476fd8e0
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-08-04 19:52:12 +0530

    [Add] Dockerized

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.7-alpine
+LABEL author=gsc2001 version=1.0
+
+RUN pip3 install pip-tools \
+    && apk add --update build-base \ 
+    libxml2-dev libxslt-dev \
+    && rm -rf /var/cache/apk/*
+
+WORKDIR /app
+COPY requirements.in  .
+RUN pip-compile requirements.in > requirements.txt \
+    && pip3 install -r requirements.txt
+
+
+COPY . /app
+EXPOSE 5000
+ENTRYPOINT ["uvicorn", "--host", "0.0.0.0", "--port", "5000", "--reload", "main:app"]
+
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,)
 from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from .utils.db_loader import connect_db, disconnect_db
 import config
-from .utils.error_handlers import http_error_handler, http_422_error_handler
 from .core import router as core_router
+from .utils.db_loader import connect_db, disconnect_db
+from .utils.error_handlers import http_error_handler, http_422_error_handler
 
 app = FastAPI()
 
